8e9d57577ccd1e24387942fe78169b9ce9166197
adammKass <adam.kascak@protonmail.com>
2026-02-11 15:38:24 +0100

    -polish, about and products page

--- src/constants.js
+++ src/constants.js
@@ -2,33 +2,37 @@ import galleryCupHorizontal from "./assets/img/gallery-cup-horizontal.jpg";
 import galleryCupVertical from "./assets/img/gallery-cup-vertical.jpg";
 import galleryHandsHorizontal from "./assets/img/gallery-hands-horizontal.jpg";
 import galleryRingHorizontal from "./assets/img/gallery-ring-horizontal.jpg";
+import feature from "./assets/img/feature.jpg";
 
 import iconAlabastron from "./assets/icon-alabastron.svg";
 import iconHydria from "./assets/icon-hydria.svg";
 import iconKrater from "./assets/icon-krater.svg";
 import iconPsykter from "./assets/icon-psykter.svg";
 import iconSkyphos from "./assets/icon-skyphos.svg";
+import jana from "./assets/img/team-jana.webp";
+import richard from "./assets/img/team-richard.webp";
+import michal from "./assets/img/team-michal.webp";
 
 export const navlinks = [
   {
     name: "Produkty",
-    path: "#products",
+    path: "/#products",
   },
   {
     name: "O nás",
-    path: "#about",
+    path: "/#about",
   },
   {
     name: "Partneri",
-    path: "#partners",
+    path: "/#partners",
   },
   {
     name: "Recenzie",
-    path: "#testimonials",
+    path: "/#testimonials",
   },
   {
     name: "Galéria",
-    path: "#gallery",
+    path: "/#gallery",
   },
 ];
 
@@ -57,6 +61,31 @@ export const products = [
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     price: "12,00 €",
     img: iconAlabastron,
+    imgFeature: feature,
+    descriptionFull:
+      "Et qui Lorem duis minim in eiusmod sint. Ex adipisicing ex ex ad laborum laboris elit laborum ullamco et sint amet sint exercitation. Id sint aute anim magna officia nisi est tempor nulla enim ea.",
+    services: [
+      {
+        title: "Dizajn",
+        description:
+          "Labore sint pariatur dolore adipisicing. Magna in enim laborum Lorem eu. Incididunt incididunt commodo ea velit est do id sunt.",
+      },
+      {
+        title: "Modelovanie",
+        description:
+          "Ut nisi cillum proident elit nostrud id consequat eu labore ut duis proident ad. Aliquip enim ea ea labore sint magna aliqua nostrud veniam.",
+      },
+      {
+        title: "Maľba",
+        description:
+          "Reprehenderit mollit esse officia irure sunt irure voluptate do dolor est ullamco Lorem.",
+      },
+      {
+        title: "Doručenie",
+        description:
+          "In ea et nostrud consectetur fugiat ad. Proident commodo fugiat aliquip duis reprehenderit nulla. Lorem sit non veniam consequat pariatur.",
+      },
+    ],
   },
   {
     name: "Hydria",
@@ -64,6 +93,31 @@ export const products = [
       "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     price: "15,00 €",
     img: iconHydria,
+    imgFeature: feature,
+    descriptionFull:
+      "Et qui Lorem duis minim in eiusmod sint. Ex adipisicing ex ex ad laborum laboris elit laborum ullamco et sint amet sint exercitation. Id sint aute anim magna officia nisi est tempor nulla enim ea.",
+    services: [
+      {
+        title: "Dizajn",
+        description:
+          "Labore sint pariatur dolore adipisicing. Magna in enim laborum Lorem eu. Incididunt incididunt commodo ea velit est do id sunt.",
+      },
+      {
+        title: "Modelovanie",
+        description:
+          "Ut nisi cillum proident elit nostrud id consequat eu labore ut duis proident ad. Aliquip enim ea ea labore sint magna aliqua nostrud veniam.",
+      },
+      {
+        title: "Maľba",
+        description:
+          "Reprehenderit mollit esse officia irure sunt irure voluptate do dolor est ullamco Lorem.",
+      },
+      {
+        title: "Doručenie",
+        description:
+          "In ea et nostrud consectetur fugiat ad. Proident commodo fugiat aliquip duis reprehenderit nulla. Lorem sit non veniam consequat pariatur.",
+      },
+    ],
   },
   {
     name: "Krater",
@@ -71,6 +125,31 @@ export const products = [
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
     price: "18,00 €",
     img: iconKrater,
+    imgFeature: feature,
+    descriptionFull:
+      "Et qui Lorem duis minim in eiusmod sint. Ex adipisicing ex ex ad laborum laboris elit laborum ullamco et sint amet sint exercitation. Id sint aute anim magna officia nisi est tempor nulla enim ea.",
+    services: [
+      {
+        title: "Dizajn",
+        description:
+          "Labore sint pariatur dolore adipisicing. Magna in enim laborum Lorem eu. Incididunt incididunt commodo ea velit est do id sunt.",
+      },
+      {
+        title: "Modelovanie",
+        description:
+          "Ut nisi cillum proident elit nostrud id consequat eu labore ut duis proident ad. Aliquip enim ea ea labore sint magna aliqua nostrud veniam.",
+      },
+      {
+        title: "Maľba",
+        description:
+          "Reprehenderit mollit esse officia irure sunt irure voluptate do dolor est ullamco Lorem.",
+      },
+      {
+        title: "Doručenie",
+        description:
+          "In ea et nostrud consectetur fugiat ad. Proident commodo fugiat aliquip duis reprehenderit nulla. Lorem sit non veniam consequat pariatur.",
+      },
+    ],
   },
   {
     name: "Psykter",
@@ -78,6 +157,31 @@ export const products = [
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     price: "20,00 €",
     img: iconPsykter,
+    imgFeature: feature,
+    descriptionFull:
+      "Et qui Lorem duis minim in eiusmod sint. Ex adipisicing ex ex ad laborum laboris elit laborum ullamco et sint amet sint exercitation. Id sint aute anim magna officia nisi est tempor nulla enim ea. Commodo elit id et proident amet cupidatat et mollit. Tempor nulla commodo minim dolore fugiat magna ea consequat labore fugiat cillum aliqua. Eu reprehenderit nostrud ullamco labore cupidatat excepteur est id. Esse sit deserunt tempor nostrud duis Lorem. Labore nisi laborum consectetur non excepteur mollit cupidatat ex dolore do laborum do esse.",
+    services: [
+      {
+        title: "Dizajn",
+        description:
+          "Labore sint pariatur dolore adipisicing. Magna in enim laborum Lorem eu. Incididunt incididunt commodo ea velit est do id sunt.",
+      },
+      {
+        title: "Modelovanie",
+        description:
+          "Ut nisi cillum proident elit nostrud id consequat eu labore ut duis proident ad. Aliquip enim ea ea labore sint magna aliqua nostrud veniam.",
+      },
+      {
+        title: "Maľba",
+        description:
+          "Reprehenderit mollit esse officia irure sunt irure voluptate do dolor est ullamco Lorem.",
+      },
+      {
+        title: "Doručenie",
+        description:
+          "In ea et nostrud consectetur fugiat ad. Proident commodo fugiat aliquip duis reprehenderit nulla. Lorem sit non veniam consequat pariatur.",
+      },
+    ],
   },
   {
     name: "Skyphos",
@@ -85,6 +189,31 @@ export const products = [
       "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     price: "14,00 €",
     img: iconSkyphos,
+    imgFeature: feature,
+    descriptionFull:
+      "Et qui Lorem duis minim in eiusmod sint. Ex adipisicing ex ex ad laborum laboris elit laborum ullamco et sint amet sint exercitation. Id sint aute anim magna officia nisi est tempor nulla enim ea.",
+    services: [
+      {
+        title: "Dizajn",
+        description:
+          "Labore sint pariatur dolore adipisicing. Magna in enim laborum Lorem eu. Incididunt incididunt commodo ea velit est do id sunt.",
+      },
+      {
+        title: "Modelovanie",
+        description:
+          "Ut nisi cillum proident elit nostrud id consequat eu labore ut duis proident ad. Aliquip enim ea ea labore sint magna aliqua nostrud veniam.",
+      },
+      {
+        title: "Maľba",
+        description:
+          "Reprehenderit mollit esse officia irure sunt irure voluptate do dolor est ullamco Lorem.",
+      },
+      {
+        title: "Doručenie",
+        description:
+          "In ea et nostrud consectetur fugiat ad. Proident commodo fugiat aliquip duis reprehenderit nulla. Lorem sit non veniam consequat pariatur.",
+      },
+    ],
   },
 ];
 
@@ -105,5 +234,66 @@ export const testimonials = [
   {
     name: "Robert Johnson",
     text: "We shipped faster, ranked higher, and slept better. Astro is a win.",
+  },
+];
+
+export const aboutStatistics = [
+  {
+    number: "2012",
+    text: "Založenie spoločnosti",
+  },
+  {
+    number: "120+",
+    text: "Spokojných zákazníkov",
+  },
+
+  {
+    number: "250k",
+    text: "Vytvorených nádob",
+  },
+  {
+    number: "1M$",
+    text: "Zisk",
+  },
+];
+
+export const aboutValues = [
+  {
+    icon: "tabler:brand-baidu",
+    span: "Zvieratká máme radi!",
+    text: "Eu duis velit nostrud aliqua. Reprehenderit mollit commodo proident sunt occaecat sunt ut.",
+  },
+  {
+    icon: "tabler:brand-gravatar",
+    span: "Dodanie načas",
+    text: "Est in elit cillum fugiat tempor nostrud amet. Et dolore occaecat labore ut labore magna pariatur ad minim reprehenderit ex voluptate.",
+  },
+  {
+    icon: "tabler:brush",
+    span: "Umelecké cvičenie",
+    text: "Ea mollit consectetur Lorem sunt deserunt ipsum eiusmod tempor Lorem fugiat nisi cillum.",
+  },
+  {
+    icon: "tabler:bus",
+    span: "Dodanie až domov",
+    text: "Enim proident nisi quis sint nisi esse cillum esse ea culpa irure eu. Irure ea laborum nisi excepteur velit proident.",
+  },
+];
+
+export const aboutTeam = [
+  {
+    name: "Jana Rýchla",
+    description: "Maľba",
+    img: jana,
+  },
+  {
+    name: "Richard Rýchly",
+    description: "Návrh",
+    img: richard,
+  },
+  {
+    name: "Michal Kováč",
+    description: "Modelovanie",
+    img: michal,
   },
 ];
